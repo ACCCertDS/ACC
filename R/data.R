648d82b961eca1eeb8072d7da09919dc07fbee23
Alexandre <alexandre.atthar@gmail.com>
2020-12-20 23:31:30 +0100

    Ajout des données dans le package

---
+++ R/data.R
@@ -0,0 +1,31 @@
+#' Caracteristiques
+#'
+#' Ajouter la description de la table caracteristiques
+#'
+#' @format Un data.frame avec les variables :
+#'   ...
+"caracteristiques"
+
+#' Usagers
+#'
+#' Ajouter la description de la table usagers
+#'
+#' @format Un data.frame avec les variables :
+#'   ...
+"usagers"
+
+#' Lieux
+#'
+#' Ajouter la description de la table lieux
+#'
+#' @format Un data.frame avec les variables :
+#'   ...
+"lieux"
+
+#' Vehicules
+#'
+#' Ajouter la description de la table vehicules
+#'
+#' @format Un data.frame avec les variables :
+#'   ...
+"vehicules"
